fa44df0e59160997a87430897857e7768f7001cb
umutdr <umutdr98@gmail.com>
2019-04-24 11:06:40 +0300

    Add project files.

---
+++ XMLDataReadApp1/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 21
+        Me.ListBox1.Location = New System.Drawing.Point(18, 19)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(313, 382)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 468)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ XMLDataReadApp1/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Xml
+
+Public Class Form1
+    Dim xmlDoc As New XmlDocument()
+    Dim xmlNodeList As XmlNodeList
+    Dim xmlNode As XmlNode
+
+    Dim sicil As String
+    Dim ad As String
+    Dim gorev As String
+    Dim ucret As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            xmlDoc.Load("isciler.xml")
+            xmlNodeList = xmlDoc.GetElementsByTagName("isci")
+
+            For Each xmlNode In xmlNodeList
+                sicil = xmlNode.Attributes("sicil").Value
+                ad = xmlNode("ad").FirstChild.Value
+                gorev = xmlNode("gorev").FirstChild.Value
+                ucret = xmlNode("ucret").FirstChild.Value
+
+                ListBox1.Items.Add("İşçi Sicil: " & sicil)
+                ListBox1.Items.Add("Ad: " & ad)
+                ListBox1.Items.Add("Görev: " & gorev)
+                ListBox1.Items.Add("Ücret: " & ucret)
+                ListBox1.Items.Add("-")
+            Next
+        Catch ex As Exception
+            MsgBox("Veri Okurken Hata: " & ex.Message)
+        End Try
+    End Sub
+End Class
